7669a7363fbe7b1d8d0203291b77f3f9c924432a
Muhammad Riki Agustin <mriki.agustin@gmail.com>
2022-02-02 03:26:16 +0700

    hasil2 pdf residu - font size 8px

--- application/modules/work/views/hasil2_pdf_residu.php
+++ application/modules/work/views/hasil2_pdf_residu.php
@@ -27,7 +27,7 @@
 		<i style="font-size:12px">Test Result</i>
 </div>
 <br>
-<table border="1" cellpadding="2" style="text-align:center;font-size:10px;">
+<table border="1" cellpadding="2" style="text-align:center;font-size:8px;">
 	<tr>
 		<td style="width:5%">No.</td>
 		<td style="width:20%">Parameter
